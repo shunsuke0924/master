1e086248edce9a6a9946dfae86615cd97d6f065e
Shunsuke Nakamura <shunsuke20010924@gmail.com>
2025-05-26 20:19:15 +0900

    修正2（5/26）

--- javascript/JavaScript.js
+++ javascript/JavaScript.js
@@ -1,3 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const hero = document.getElementById('hero');
+  hero.classList.add('visible');
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const fadeItems = document.querySelectorAll('.fade-item');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target); 
+      }
+    });
+  });
+
+  fadeItems.forEach(item => {
+    observer.observe(item);
+  });
+});
+
+
 const select = document.getElementById("age");
             for (let i = 18; i <= 99; i++) {
                 const option = document.createElement("option");
